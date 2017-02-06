5698548a03e99b3c2e640c7ae011595075185308
Ilya Yaroshenko <ilyayaroshenko@gmail.com>
2017-02-06 13:31:06 +0700

    remove unused code in conv.d

--- source/mir/conv.d
+++ source/mir/conv.d
@@ -1,54 +1,6 @@
 module mir.conv;
 
-import std.range.primitives;
 import std.traits;
-import std.meta;
-public import std.ascii : LetterCase;
-
-
-package template convFormat()
-{
-    import std.format : format;
-    alias convFormat = format;
-}
-
-/* ************* Exceptions *************** */
-
-/**
- * Thrown on conversion errors.
- */
-class ConvException : Exception
-{
-    @safe pure nothrow
-    this(string s, string fn = __FILE__, size_t ln = __LINE__)
-    {
-        super(s, fn, ln);
-    }
-}
-
-private string convError_unexpected(S)(S source)
-{
-    return source.empty ? "end of input" : text("'", source.front, "'");
-}
-
-private auto convError(S, T)(S source, string fn = __FILE__, size_t ln = __LINE__)
-{
-    return new ConvException(
-        text("Unexpected ", convError_unexpected(source),
-             " when converting from type "~S.stringof~" to type "~T.stringof),
-        fn, ln);
-}
-
-private auto convError(S, T)(S source, int radix, string fn = __FILE__, size_t ln = __LINE__)
-{
-    return new ConvException(
-        text("Unexpected ", convError_unexpected(source),
-             " when converting from type "~S.stringof~" base ", radix,
-             " to type "~T.stringof),
-        fn, ln);
-}
-
-
 
 /**
 The `to` template converts a value from one type _to another.
