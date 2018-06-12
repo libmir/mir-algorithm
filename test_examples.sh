89087379f6d0d92b3d759e70c493c2c46c195b84
Sebastian Wilzbach <seb@wilzba.ch>
2018-06-12 19:47:23 +0200

    Add simple tests extractor and validation script

---
+++ test_examples.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+set -euo pipefail
+
+if [ ! -e test_extractor.d ] ; then
+    wget https://raw.githubusercontent.com/dlang/tools/6b7ef76d679563e6fccef2a6b437008d96459e95/tests_extractor.d
+fi
+
+# extract examples
+dub ./tests_extractor.d -i source -o out
+
+# compile the examples
+for file in $(find out -name "*.d") ; do
+    echo "Testing: $file"
+    dmd -de -unittest -Isource -c -o- "$file"
+done
