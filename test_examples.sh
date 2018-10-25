12cc6ddf8151958fe20266a4db59a66a78179c75
Ilya Yaroshenko <ilyayaroshenko@gmail.com>
2018-10-25 18:18:40 +0700

    fix travis

--- test_examples.sh
+++ test_examples.sh
@@ -7,8 +7,10 @@ dub fetch dtools
 # extract examples
 dub run dtools:tests_extractor -- -i source -o out
 
+git clone https://github.com/libmir/mir-core
+
 # compile the examples
 for file in $(find out -name "*.d") ; do
     echo "Testing: $file"
-    $DMD -de -unittest -Isource -c -o- "$file"
+    $DMD -de -unittest -Isource -Imir-core/source -c -o- "$file"
 done
