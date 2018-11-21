de2f0e78bc6748800af884d696e4155eced3a976
Ilya Yaroshenko <ilyayaroshenko@gmail.com>
2018-11-21 22:38:05 +0700

    ditto

--- include/mir/rcarray.h
+++ include/mir/rcarray.h
@@ -20,7 +20,7 @@ private:
 public:
 
     ~mir_rcarray();
-    mir_rcarray(std::conditional<std::is_const<T>, const mir_rcarray, mir_rcarray>& rhs);
+    mir_rcarray(std::conditional<std::is_const<T>::value, const mir_rcarray, mir_rcarray>& rhs);
     bool initialize(size_t length, unsigned int alignment, bool deallocate, bool initialize);
 
     mir_slice<mir_rci<T>> asSlice();
