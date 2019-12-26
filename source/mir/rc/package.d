65e05631f2e30a81cb37374dec602a3bc1dbc656
Ilya Yaroshenko <ilyayaroshenko@gmail.com>
2019-12-26 12:18:16 +0700

    add mir.variant and mir.rc (#224)

--- source/mir/rc/package.d
+++ source/mir/rc/package.d
@@ -1,10 +1,153 @@
 /++
 $(H1 Thread-safe reference-counted arrays and pointers)
 
-Publicly imports $(MREF mir,rc,array) and $(MREF mir,rc,ptr).
+Mir provides two kinds of ref-counting pointers and two kinds of ref-counted arrays.
+
+The first kind pointer is `RCPtr`, which consists of a pointer to the context and pointer to the value.`RCPtr` supports structural and object polymorphism. It allows getting members with the same context as the root.
+The second kind is `SlimRCPtr`, which consist only from a pointer to the value. The context for `SlimRCPtr`is computed using a fixed-length memory shift from the pointer to the value.
+`SlimRCPtr` can be converted to an `RCPtr` and to an `RCArray` of the one element.
+
+`RCArray` is an array type without range primitives. It's length can't be reduced after construction.In the other hand, `Slice!(RCI!(T))` is an ndslice with all random-access range primitives.`RCI` is an iterator, which consists of `RCArray` and the pointer to the current element.
+`RCArray!T` can be converted or moved to `Slice!(RCI!(T))` using `.asSlice` or `.moveToSlice` methods respectively.
+
+$(RED `RCArray!T` aliases itself to a common D array slice. This feature may cause a segmentation fault in safe code if used without DIP1000.)
+
+`RCPtr!T` can be constructed from an element index and `RCArray!T` / `Slice!(RCI!(T))`.
+
+The package publicly imports $(MREF mir,rc,array), $(MREF mir,rc,ptr), and $(MREF mir,rc,slim_ptr).
+
+See_also: $(MREF mir,ndslice).
 +/
 module mir.rc;
 
 public import mir.rc.array;
 public import mir.rc.ptr;
 public import mir.rc.slim_ptr;
+
+import mir.ndslice.slice;
+
+/++
+Returns: shared pointer constructed from the slim shared pointer.
+
+The function has zero computation cost.
++/
+RCPtr!F toRCPtr(F)(return SlimRCPtr!F contextAndValue) @trusted
+{
+    typeof(return) ret;
+    ret._value = contextAndValue._value;
+    ret._context = &contextAndValue.context();
+    contextAndValue._value = null;
+    return ret;
+}
+
+///
+version(mir_test)
+@safe pure @nogc nothrow
+unittest
+{
+    import core.lifetime: move;
+    struct S
+    {
+        double e;
+    }
+    struct C
+    {
+        int i;
+        S s;
+    }
+
+    auto a = createSlimRC!C(10, S(3));
+    auto s = a.move.toRCPtr.shareMember!"s";
+    assert(s._counter == 1);
+    assert(s.e == 3);
+}
+
+/++
+Returns: shared pointer constructed with the `array`'s context and the value points to `array[index]`.
+
+The function has zero computation cost.
++/
+RCPtr!F toRCPtrAt(F)(return RCArray!F array, size_t index) @trusted
+    if (!is(R == class) && !is(R == interface))
+in {
+    assert(index < array.length, "toRCPtrAt: index should be less then array.length");
+}
+body {
+    typeof(return) ret;
+    ret._value = array._payload + index;
+    ret._context = &array.context();
+    array._payload = null;
+    return ret;
+}
+
+///
+version(mir_test)
+@safe pure @nogc nothrow
+unittest
+{
+    struct S { double e; }
+
+    auto a = RCArray!S(10);
+    a[3].e = 4;
+
+    auto s = a.toRCPtrAt(3);
+
+    assert(s._counter == 2);
+    assert(s.e == 4);
+}
+
+/// ditto
+RCPtr!F toRCPtrAt(F)(return Slice!(RCI!F) array, size_t index) @trusted
+    if (!is(R == class) && !is(R == interface))
+in {
+    assert(index < array.length, "toRCPtrAt: index should be less then array.length");
+}
+body {
+    typeof(return) ret;
+    ret._value = array._iterator._iterator + index;
+    ret._context = &array._iterator._array.context();
+    array._iterator._array._payload = null;
+    return ret;
+}
+
+///
+version(mir_test)
+@safe pure @nogc nothrow
+unittest
+{
+    struct S { double e; }
+
+    auto a = RCArray!S(10).asSlice[5 .. $];
+    a[3].e = 4;
+
+    auto s = a.toRCPtrAt(3);
+
+    assert(s._counter == 2);
+    assert(s.e == 4);
+}
+
+/++
+Returns: RC array length of one constructed from the slim shared pointer.
+
+The function has zero computation cost.
++/
+RCArray!F toRCArray(F)(return SlimRCPtr!F context) @trusted
+{
+    typeof(return) ret;
+    ret._payload = context._value;
+    context._value = null;
+    return ret;
+}
+
+///
+version(mir_test)
+@safe pure @nogc nothrow
+unittest
+{
+    struct S { double e; }
+
+    auto a = createSlimRC!S(4).toRCArray;
+    assert(a._counter == 1);
+    assert(a.length == 1);
+    assert(a[0].e == 4);
+}
---
+++ source/mir/variant.d
@@ -0,0 +1,206 @@
+/++
+This module implements a generic variant type.
++/
+module mir.variant;
+
+private static immutable variantExceptionMsg = "mir.variant: the variant stores other type then requested.";
+private static immutable variantNulllExceptionMsg = "mir.variant: the variant is empty and doesn't store any value.";
+private static immutable variantMemberExceptionMsg = "mir.variant: the variant is stores the type that isn't compatible with the user proveded visitor and arguments.";
+
+version (D_Exceptions)
+{
+    private static immutable variantException = new Exception(variantExceptionMsg);
+    private static immutable variantNulllException = new Exception(variantNulllExceptionMsg);
+    private static immutable variantMemberException = new Exception(variantMemberExceptionMsg);
+}
+
+/++
+Variant Type (aka Algebraic Type) with clever member access.
+
+Compatible with BetterC mode.
++/
+struct Variant(Types...)
+    if (Types.length)
+{
+    import core.lifetime: move;
+    import mir.conv: emplaceRef;
+    import std.meta: anySatisfy;
+    import std.traits: Largest, hasElaborateDestructor, hasElaborateAssign;
+
+    private alias _Types = Types;
+
+    private void[Largest!Types.sizeof] payload = void;
+    private uint type; // 0 for unininit value, index = type - 1
+    private enum hasDestructor = anySatisfy!(hasElaborateDestructor, Types);
+pure nothrow @nogc:
+    static if (hasDestructor)
+    ~this()
+    {
+        S: switch (type)
+        {
+            static foreach (i, T; Types) static if (hasElaborateDestructor!T)
+            {
+                case i + 1:
+                    .destroy!false(trustedGet!T);
+                    break S;
+            }
+            default: break;
+        }
+        type = 0;
+    }
+
+    static if (anySatisfy!(hasElaborateAssign, Types))
+    this(this)
+    {
+        S: switch (type)
+        {
+            static foreach (i, T; Types) static if (hasElaborateAssign!T)
+            {
+                case i + 1:
+                    __ctor(trustedGet!T);
+                    break S;
+            }
+            default: break;
+        }
+    }
+
+    ///
+    this(typeof(null))
+    {
+    }
+
+    ///
+    void opAssign(typeof(null))
+    {
+        static if (hasDestructor)
+            __dtor;
+        type = 0;
+    }
+
+    ///
+    void opAssign(Variant value)
+    {
+        static if (hasDestructor)
+            __dtor;
+        emplaceRef(this, move(value));
+    }
+
+    static foreach (i, T; Types)
+    ///
+    void opAssign(T value)
+    {
+        static if (hasDestructor)
+            __dtor;
+        type = i + 1;
+        emplaceRef(trustedGet!T, move(value));
+    }
+
+    static foreach (i, T; Types)
+    ///
+    this(T value)
+    {
+        type = i + 1;
+        emplaceRef(trustedGet!T, move(value));
+    }
+
+    static foreach (i, T; Types)
+    ///
+    ref inout(E) get(E : T)() @property return inout
+    {
+        import mir.utility: _expect;
+        if (_expect(i + 1 != type, false))
+        {
+            if (i == 0)
+            {
+                version(D_Exceptions)
+                    throw variantNulllException;
+                else
+                    assert(0, variantNulllExceptionMsg);
+            }
+            version(D_Exceptions)
+                throw variantException;
+            else
+                assert(0, variantExceptionMsg);
+        }
+        return trustedGet!T;
+    }
+
+    static foreach (i, T; Types)
+    /// Zero cost always nothrow `get` alternative
+    ref inout(E) trustedGet(E : T)() @trusted @property return inout nothrow
+    {
+        assert (i + 1 == type);
+        return *cast(inout(T)*)payload.ptr;
+    }
+
+    ///
+    bool empty() const @property
+    {
+        return type == 0;
+    }
+}
+
+/++
+Params:
+    visitor = a function name alias
+    forceAllTypes = if `true` checks at compile time, that the member can be called for all types.
++/
+template visit(alias visitor, bool forceAllTypes = true)
+{
+    import std.traits: Unqual;
+    ///
+    auto ref visit(V, Args)(auto ref V variant, auto ref Args args)
+        if (is(Unqual!V : Variant!Types, Types))
+    {
+        import mir.functional: forward;
+        switch (variant.type)
+        {
+            case 0:
+                version(D_Exceptions)
+                    throw variantNulllException;
+                else
+                    assert(0, variantNulllExceptionMsg);
+            static foreach (i, T; V._Types)
+            static if (forceAllTypes || __traits(compiles, { return visitor(variant.trustedGet!T, forward!args); }))
+            {
+                case i + 1:
+                    return visitor(variant.trustedGet!T, forward!args);
+            }
+            else
+            static if (forceAllTypes)
+                static assert(0, V.stringof ~ ": the visitor cann't be caled with type (first argument) " ~ T.stringof ~ " and additional arguments " ~ Args.stringof);
+            default:
+                version(D_Exceptions)
+                    throw variantMemberException;
+                else
+                    assert(0, variantMemberExceptionMsg);
+        }
+    }
+}
+
+/++
+Params:
+    visitor = a function name alias
++/
+template optionalVisit(alias visitor)
+{
+    import std.traits: Unqual;
+    ///
+    bool optionalVisit(Result, V, Args)(out Result result, auto ref V variant, auto ref Args args)
+        if (is(Unqual!V : Variant!Types, Types))
+    {
+        import mir.functional: forward;
+        switch (variant.type)
+        {
+            static foreach (i, T; V._Types)
+            static if (__traits(compiles, { result = visitor(variant.trustedGet!T, forward!args); }))
+            {
+                case i + 1:
+                    result = visitor(variant.trustedGet!T, forward!args);
+                    return true;
+            }
+            default:
+                return false;
+        }
+    }
+}
