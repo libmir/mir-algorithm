c6eb8e33e52c622c22331ee4961fd7c9c188c18e
ilya <ilyayaroshenko@gmail.com>
2021-11-07 13:49:57 +0700

    update lob

--- source/mir/lob.d
+++ source/mir/lob.d
@@ -2,10 +2,13 @@
 +/
 module mir.lob;
 
+import mir.serde: serdeRegister;
+
 /++
 Values of type clob are encoded as a sequence of octets that should be interpreted as text
 with an unknown encoding (and thus opaque to the application).
 +/
+@serdeRegister
 struct Clob
 {
     ///
@@ -15,6 +18,7 @@ struct Clob
 /++
 This is a sequence of octets with no interpretation (and thus opaque to the application).
 +/
+@serdeRegister
 struct Blob
 {
     ///
