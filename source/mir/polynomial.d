fada1eebee82c4b6e12f0279ddf4b2be885d0ebd
John Michael Hall <john.michael.hall@gmail.com>
2023-02-15 20:40:04 -0500

    Adjust monic to be normal function instead of return ref-counted struct

--- source/mir/polynomial.d
+++ source/mir/polynomial.d
@@ -94,7 +94,7 @@ version (mir_test) @safe pure nothrow @nogc unittest
     assert(p.opCall!2(3.3).approxEqual(d2f(3.3)));
     assert(p.opCall!2(7.2).approxEqual(d2f(7.2)));
 }
-
+/*
 /++
 Monic polynomial (leading coefficient equals 1) callable ref-counted structure.
 
@@ -157,91 +157,189 @@ struct Monic(F)
         }
     }
 }
+*/
+typeof(F.init * X.init * 1f + F.init) monicImpl(X, T, F, uint derivative = 0)(in X x, in T coefficients)
+{
+    import mir.internal.utility: Iota;
+    auto ret = cast(typeof(return))0;
+    if (coefficients.length > 0)
+    {
+        ptrdiff_t i = coefficients.length - 1;
+        assert(i >= 0);
+        ptrdiff_t j = 1;
+        static foreach (d; Iota!derivative) {
+            j *= i - d + 1;
+        }
+        ret += j;
+        typeof(cast()coefficients[0]) c;
+        if (i >= cast(ptrdiff_t)derivative) {
+            do
+            {
+                assert(i < coefficients.length);
+                ret *= x;
+                c = cast()coefficients[i];
+                static foreach (d; Iota!derivative)
+                    c *= i - d;
+                ret += c;
+            } while (--i >= cast(ptrdiff_t)derivative);
+        }
+    }
+    return ret;
+}
+
+/++
+Monic polynomial (leading coefficient equals 1).
+
+Params:
+    F = controls type of output
+    derivative = order of derivatives (default = 0)
+
+Returns:
+    The quantile of all the elements in the input at probability `p`.
+
+
+See_also:
+    $(WEB en.wikipedia.org/wiki/Monic_polynomial, Monic polynomial).
+
++/
+template monic(F, uint derivative = 0)
+{
+    import mir.ndslice.slice: Slice, SliceKind;
+
+    /++
+    Params:
+        x = value to evaluate
+        coefficients = coefficients of polynomial
+    +/
+    typeof(F.init * X.init * 1f + F.init) monic(X, T : Slice!(const(U)*, 1, sliceKind), U, SliceKind sliceKind)
+        (in X x, T coefficients)
+    {
+        import core.lifetime: move;
+        return monicImpl!(X, T, F, derivative)(x, coefficients.move);
+    }
+
+    /// ditto
+    typeof(F.init * X.init * 1f + F.init) monic(X, T)(in X x, scope const T[] coefficients...)
+    {
+        return monicImpl!(X, typeof(coefficients), F, derivative)(x, coefficients);
+    }
+}
 
 /// ditto
-Monic!F monic(F)(RCArray!(const F) coefficients)
+template monic(uint derivative = 0)
 {
-    import core.lifetime: move;
-    return typeof(return)(coefficients.move);
+    import mir.ndslice.slice: Slice, SliceKind;
+
+    /// ditto
+    typeof(U.init * X.init * 1f + U.init) monic(X, T : Slice!(const(U)*, 1, sliceKind), U, SliceKind sliceKind)
+        (in X x, T coefficients)
+    {
+        import core.lifetime: move;
+        return monicImpl!(X, T, U, derivative)(x, coefficients.move);
+    }
+
+    /// ditto
+    typeof(T.init * X.init * 1f + T.init) monic(X, T)(in X x, scope const T[] coefficients...)
+    {
+        return monicImpl!(X, typeof(coefficients), T, derivative)(x, coefficients);
+    }
 }
 
 ///
 version (mir_test) @safe pure nothrow @nogc unittest
 {
     import mir.math.common: approxEqual;
-    import mir.rc.array;
-    auto a = rcarray!(const double)(3.0, 4.5, 1.9, 2);
-    auto m = a.monic;
+    import mir.ndslice.slice: sliced;
+    static immutable a = [3.0, 4.5, 1.9, 2];
+    auto x = a.sliced;
 
     alias f = (x) =>       3.0 +     4.5 * x^^1 +   1.9 * x^^2 + 2 * x^^3 + x^^4;
     alias df = (x) =>      4.5 + 2 * 1.9 * x^^1 + 3 * 2 * x^^2 + 4 * x^^3;
     alias d2f = (x) => 2 * 1.9 +   6 * 2 * x^^1 + 3 * 4 * x^^2;
 
-    assert(m(3.3).approxEqual(f(3.3)));
-    assert(m(7.2).approxEqual(f(7.2)));
+    assert(monic(3.3, x).approxEqual(f(3.3)));
+    assert(monic(7.2, x).approxEqual(f(7.2)));
 
-    assert(m.opCall!1(3.3).approxEqual(df(3.3)));
-    assert(m.opCall!1(7.2).approxEqual(df(7.2)));
+    assert(monic!1(3.3, x).approxEqual(df(3.3)));
+    assert(monic!1(7.2, x).approxEqual(df(7.2)));
 
-    assert(m.opCall!2(3.3).approxEqual(d2f(3.3)));
-    assert(m.opCall!2(7.2).approxEqual(d2f(7.2)));
+    assert(monic!2(3.3, x).approxEqual(d2f(3.3)));
+    assert(monic!2(7.2, x).approxEqual(d2f(7.2)));
+    
+    // can also control the type
+    assert(monic!real(3.3, x).approxEqual(f(3.3)));
 }
 
+/// Dynamic array
+version (mir_test) @safe pure nothrow unittest
+{
+    import mir.math.common: approxEqual;
+    double[] x = [3.0, 4.5, 1.9, 2];
+
+    alias f = (x) =>       3.0 +     4.5 * x^^1 +   1.9 * x^^2 + 2 * x^^3 + x^^4;
+
+    assert(monic(3.3, x).approxEqual(f(3.3)));
+    assert(monic!real(3.3, x).approxEqual(f(3.3)));
+}
+
+// Check coefficient.length = 3
 version (mir_test) @safe pure nothrow @nogc unittest
 {
     import mir.math.common: approxEqual;
-    import mir.rc.array;
-    auto a = rcarray!(const double)(3.0, 4.5, 1.9);
-    auto m = a.monic;
+    import mir.ndslice.slice: sliced;
+    static immutable a = [3.0, 4.5, 1.9];
+    auto x = a.sliced;
 
     alias f = (x) =>       3.0 +     4.5 * x^^1 + 1.9 * x^^2 + x^^3;
     alias df = (x) =>      4.5 + 2 * 1.9 * x^^1 +   3 * x^^2;
     alias d2f = (x) => 2 * 1.9 +       6 * x^^1;
 
-    assert(m(3.3).approxEqual(f(3.3)));
-    assert(m(7.2).approxEqual(f(7.2)));
+    assert(monic(3.3, x).approxEqual(f(3.3)));
+    assert(monic(7.2, x).approxEqual(f(7.2)));
 
-    assert(m.opCall!1(3.3).approxEqual(df(3.3)));
-    assert(m.opCall!1(7.2).approxEqual(df(7.2)));
+    assert(monic!1(3.3, x).approxEqual(df(3.3)));
+    assert(monic!1(7.2, x).approxEqual(df(7.2)));
 
-    assert(m.opCall!2(3.3).approxEqual(d2f(3.3)));
-    assert(m.opCall!2(7.2).approxEqual(d2f(7.2)));
+    assert(monic!2(3.3, x).approxEqual(d2f(3.3)));
+    assert(monic!2(7.2, x).approxEqual(d2f(7.2)));
 }
 
+// Check coefficient.length = 2
 version (mir_test) @safe pure nothrow @nogc unittest
 {
     import mir.math.common: approxEqual;
-    import mir.rc.array;
-    auto a = rcarray!(const double)(3.0, 4.5);
-    auto m = a.monic;
+    import mir.ndslice.slice: sliced;
+    static immutable a = [3.0, 4.5];
+    auto x = a.sliced;
 
     alias f = (x) =>   3.0 + 4.5 * x^^1 + x^^2;
     alias df = (x) =>  4.5 +   2 * x^^1;
     alias d2f = (x) =>   2;
 
-    assert(m(3.3).approxEqual(f(3.3)));
-    assert(m(7.2).approxEqual(f(7.2)));
+    assert(monic(3.3, x).approxEqual(f(3.3)));
+    assert(monic(7.2, x).approxEqual(f(7.2)));
 
-    assert(m.opCall!1(3.3).approxEqual(df(3.3)));
-    assert(m.opCall!1(7.2).approxEqual(df(7.2)));
+    assert(monic!1(3.3, x).approxEqual(df(3.3)));
+    assert(monic!1(7.2, x).approxEqual(df(7.2)));
 
-    assert(m.opCall!2(3.3).approxEqual(d2f(3.3)));
-    assert(m.opCall!2(7.2).approxEqual(d2f(7.2)));
+    assert(monic!2(3.3, x).approxEqual(d2f(3.3)));
+    assert(monic!2(7.2, x).approxEqual(d2f(7.2)));
 }
 
+// Check coefficient.length = 1
 version (mir_test) @safe pure nothrow @nogc unittest
 {
     import mir.math.common: approxEqual;
-    import mir.rc.array;
-    auto a = rcarray!(const double)(3.0);
-    auto m = a.monic;
+    import mir.ndslice.slice: sliced;
+    static immutable a = [3.0];
+    auto x = a.sliced;
 
     alias f = (x) =>  3.0 + x^^1;
     alias df = (x) => 1;
 
-    assert(m(3.3).approxEqual(f(3.3)));
-    assert(m(7.2).approxEqual(f(7.2)));
+    assert(monic(3.3, x).approxEqual(f(3.3)));
+    assert(monic(7.2, x).approxEqual(f(7.2)));
 
-    assert(m.opCall!1(3.3).approxEqual(df(3.3)));
-    assert(m.opCall!1(7.2).approxEqual(df(7.2)));
+    assert(monic!1(3.3, x).approxEqual(df(3.3)));
+    assert(monic!1(7.2, x).approxEqual(df(7.2)));
 }
